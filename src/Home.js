7bc02c0731c48d3e4af691257b7dda9d1fa691f5
anastasiamajewski <anastasiamajewski1991@gmail.com>
2020-05-06 12:35:48 +0200

    Add questions json

--- src/Home.js
+++ src/Home.js
@@ -1,161 +1,14 @@
 import React, { useState } from 'react'
 import RatingSystem from './RatingSystem'
 import styled from 'styled-components/macro'
+import questionList from './questions.json'
 
 export default function Home() {
-  const [questions, setQuestion] = useState([
-    {
-      id: '1',
-      question: 'Hast du jemandem einen schönen Tag gewünscht?',
-      rating: 0,
-    },
-    {
-      id: '2',
-      question: 'Hast du Zeit mit deinem Hobby verbracht?',
-      rating: 0,
-    },
-    {
-      id: '3',
-      question: 'Hast du dich mit einem/er Freund/Freundin unterhalten?',
-      rating: 0,
-    },
-    {
-      id: '4',
-      question: 'Hast du deine Lieblingsmusik gehört?',
-      rating: 0,
-    },
-    {
-      id: '5',
-      question: 'Hast du Komplimente verteilt?',
-      rating: 0,
-    },
-    {
-      id: '6',
-      question: 'Hast du soziale Medien bewusst genutzt?',
-      rating: 5,
-    },
-    {
-      id: '7',
-      question: 'Hast du genug geschlafen?',
-      rating: 5,
-    },
-    {
-      id: '8',
-      question: 'Bist du mit einem positiven Gedanken aus dem Bett gestiegen?',
-      rating: 5,
-    },
-    {
-      id: '9',
-      question: 'Warst du draußen an der frischen Luft?',
-      rating: 5,
-    },
-    {
-      id: '10',
-      question: 'Hast du dich genügend bewegt?',
-      rating: 5,
-    },
-    {
-      id: '11',
-      question: 'Hast du genug Wasser getrunken?',
-      rating: 5,
-    },
-    {
-      id: '12',
-      question: 'Hast du Sport getrieben?',
-      rating: 5,
-    },
-    {
-      id: '13',
-      question: 'Konntest du stressige Situationen vermeiden?',
-      rating: 5,
-    },
-    {
-      id: '14',
-      question: 'Hast du dir Zeit fürs Frühstück genommen?',
-      rating: 5,
-    },
-    {
-      id: '15',
-      question: 'Hast du Obst oder Gemüse gegessen?',
-      rating: 5,
-    },
-    {
-      id: '16',
-      question: 'Konntest du auf Fleisch verzichten?',
-      rating: 5,
-    },
-    {
-      id: '17',
-      question: 'Hast du dein Essen bewusst genossen ohne Ablenkungen?',
-      rating: 5,
-    },
-    {
-      id: '18',
-      question: 'Hast du dir Zeit fürs Kochen genommen?',
-      rating: 5,
-    },
-    {
-      id: '19',
-      question: 'Konntest du auf süße und fettige Sünden verzichten?',
-      rating: 5,
-    },
-    {
-      id: '20',
-      question: 'Hast du dir heute ein Kompliment gemacht?',
-      rating: 5,
-    },
-    {
-      id: '21',
-      question: 'Hast du heute herzhaft gelacht?',
-      rating: 5,
-    },
-    {
-      id: '22',
-      question: 'Hast du dir heute irgendetwas schönes gegönnt?',
-      rating: 5,
-    },
-    {
-      id: '23',
-      question: 'Hast du deinen Ärger im Griff gehabt?',
-      rating: 5,
-    },
-    {
-      id: '24',
-      question: 'Hast du dich heute gelobt für das, was du geschafft hast?',
-      rating: 5,
-    },
-    {
-      id: '25',
-      question: 'Warst du rechtzeitig bei der Arbeit?',
-      rating: 5,
-    },
-    {
-      id: '26',
-      question: 'Hast du deine Kollegen freundlich begrüßt?',
-      rating: 5,
-    },
-    {
-      id: '27',
-      question:
-        'Hast du dich mit deinen Kollegen auch über Privates unterhalten?',
-      rating: 5,
-    },
-    {
-      id: '28',
-      question:
-        'Hast du es geschafft deine To-dos für den Arbeitstag zu erledigen?',
-      rating: 5,
-    },
-    {
-      id: '29',
-      question: 'War deine Laune bei der Arbeit gut?',
-      rating: 5,
-    },
-  ])
+  const [questions, setQuestion] = useState([])
 
   return (
     <StyledMain>
-      {questions.map((question) => (
+      {questionList.map((question) => (
         <QuestionCard key={question.id}>
           <p>{question.question}</p>
           <RatingSystem
@@ -168,13 +21,13 @@ export default function Home() {
     </StyledMain>
   )
   function setRating(id, newRating) {
-    const index = questions.findIndex((question) => question.id === id)
-    const question = questions[index]
+    const index = questionList.findIndex((question) => question.id === id)
+    const question = questionList[index]
     question.rating = newRating
     //
     setQuestion([
-      ...questions.slice(0, index, -1),
-      ...questions.slice(index, questions.length),
+      ...questionList.slice(0, index, -1),
+      ...questionList.slice(index, questions.length),
     ])
   }
 }
